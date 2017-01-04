23991d792a7a24199d89f57a35815c82634d7dd4
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-01-04 07:08:19 +0200

    stop coloring tree in dendrogram viewer

--- cnviewer/utils/model.py
+++ cnviewer/utils/model.py
@@ -64,7 +64,7 @@ class DataModel(DataLoader):
         if self.Z is not None:
             return
         self.Z = dendrogram(
-            self.lmat, ax=None, no_plot=True, color_threshold=0)
+            self.lmat, ax=None, no_plot=True, color_threshold=99999999)
         self.icoord = np.array(self.Z['icoord'])
         self.dcoord = np.array(self.Z['dcoord'])
         self.min_x = np.min(self.icoord)
