ceb725dd0d71537ed9306a0123c0748326a81aaf
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-02-08 11:35:40 +0200

    add legend for heatmap colormaps
    Task-Url: https://app.asana.com/0/185270059359143/240045499057409

--- cnviewer/tkutils/heatmap_legend.py
+++ cnviewer/tkutils/heatmap_legend.py
@@ -60,11 +60,32 @@ class HeatmapLegend(object):
         frame.grid_columnconfigure(0, weight=1)
         frame.grid_rowconfigure(20, weight=1)
 
-#         self.container = tk.Frame(self.canvas)
-#         self.canvas.create_window((0, 0), window=self.container, anchor='nw')
+        self.container = tk.Frame(self.canvas, background='white')
+        self.canvas.create_window((0, 0), window=self.container, anchor='nw')
 
         image = Image.new('RGB', size=(20, 20), color=(255, 0, 0))
         self.bitmap = ImageTk.PhotoImage(image=image)
-        self.canvas.create_image(0, 0, image=self.bitmap)
+
+        def click_callback(val):
+            return lambda event: print("click_callback({}) called".format(val))
+
+        l = ttk.Label(
+            self.container,
+            text='text1',
+            image=self.bitmap,
+            compound=tk.LEFT)
+        l.pack()
+        l.bind('<Double-Button-1>', click_callback(1))
+
+        image = Image.new('RGB', size=(20, 20), color=(0, 255, 0))
+        self.bitmap2 = ImageTk.PhotoImage(image=image)
+
+        l = ttk.Label(
+            self.container,
+            text='text1',
+            image=self.bitmap2,
+            compound=tk.LEFT)
+        l.pack()
+        l.bind('<Double-Button-1>', click_callback(2))
 
         configure_update(None)
