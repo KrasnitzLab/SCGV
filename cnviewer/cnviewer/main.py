499db5d00c150ed520f1737d1b226d0e92acbc8b
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-02-21 14:13:32 +0200

    split the code to model, controller and viewer
    Task-Url: https://app.asana.com/0/185270059359143/272184146155229

--- cnviewer/cnviewer/main.py
+++ cnviewer/cnviewer/main.py
@@ -4,133 +4,11 @@ Created on Feb 8, 2017
 @author: lubo
 '''
 from tkutils.tkimport import *  # @UnusedWildImport
-from tkutils.canvas_ui import CanvasWindow
-import matplotlib.pyplot as plt
 
-
-from tkutils.profiles_ui import ProfilesUi
-from tkutils.open_ui import OpenUi
-from tkutils.pinmat_ui import PinmatUi
-from tkutils.sectors_ui import SectorsUi, SectorsWindow
-from tkutils.sectors_legend2 import SectorsLegend2
-from tkutils.heatmap_legend import HeatmapLegend
+from tkutils.sectors_ui import SectorsWindow
 from models.sector_model import SingleSectorDataModel
 from controllers.controller import MainController
-from views.dendrogram import DendrogramViewer
-from views.clone import CloneViewer
-from views.heatmap import HeatmapViewer
-from views.sector import SectorViewer
-from views.gate import GateViewer
-from views.multiplier import MultiplierViewer
-from views.error import ErrorViewer
-from views.sample import SampleViewer
-
-
-class MainWindow(object):
-
-    def __init__(self, master, controller):
-        self.master = master
-        self.controller = controller
-        self.controller.register_on_model_callback(self.on_model_callback)
-
-    def build_ui(self):
-
-        self.main = CanvasWindow(self.master, self.controller)
-
-        profiles = ProfilesUi(self.main.button_ext, self.main)
-        profiles.build_ui()
-
-        pinmat = PinmatUi(self.main.button_ext)
-        pinmat.build_ui()
-
-        sectors = SectorsUi(self.main.button_ext)
-        sectors.build_ui()
-
-        sectors_legend = SectorsLegend2(self.main.legend_ext)
-        sectors_legend.build_ui(row=10)
-        # sectors_legend.register_show_single_sector_callback(show_single_sector)
-
-        heatmap_legend = HeatmapLegend(self.main.legend_ext)
-        heatmap_legend.build_ui()
-        heatmap_legend.show_legend()
-
-        open_buttons = OpenUi(self.main.button_ext, self.controller)
-        open_buttons.build_ui()
-
-    def on_model_callback(self, model):
-        self.fig = self.main.fig
-        self.model = model
-
-        ax_dendro = self.fig.add_axes([0.1, 0.775, 0.8, 0.175], frame_on=True)
-        dendro_viewer = DendrogramViewer(self.model)
-        dendro_viewer.draw_dendrogram(ax_dendro)
-
-        ax_clone = self.fig.add_axes(
-            [0.1, 0.7625, 0.8, 0.0125], frame_on=True, sharex=ax_dendro)
-        clone_viewer = CloneViewer(self.model)
-        clone_viewer.draw_clone(ax_clone)
-        ax_subclone = self.fig.add_axes(
-            [0.1, 0.75, 0.8, 0.0125], frame_on=True, sharex=ax_dendro)
-        clone_viewer.draw_subclone(ax_subclone)
-
-        ax_heat = self.fig.add_axes(
-            [0.1, 0.20, 0.8, 0.55], frame_on=True, sharex=ax_dendro)
-
-        heatmap_viewer = HeatmapViewer(self.model)
-        heatmap_viewer.draw_heatmap(ax_heat)
-        # heatmap_viewer.draw_legend()
-
-        ax_sector = self.fig.add_axes(
-            [0.1, 0.175, 0.8, 0.025], frame_on=True, sharex=ax_dendro)
-        # draw sector bar
-        sector_viewer = SectorViewer(self.model)
-        sector_viewer.draw_sector(ax_sector)
-
-        ax_gate = self.fig.add_axes(
-            [0.1, 0.150, 0.8, 0.025], frame_on=True, sharex=ax_dendro)
-        gate_viewer = GateViewer(self.model)
-        gate_viewer.draw_ploidy(ax_gate)
-
-        ax_multiplier = self.fig.add_axes(
-            [0.1, 0.125, 0.8, 0.025], frame_on=True, sharex=ax_dendro)
-        multiplier_viewer = MultiplierViewer(self.model)
-        multiplier_viewer.draw_multiplier(ax_multiplier)
-
-        ax_error = self.fig.add_axes(
-            [0.1, 0.10, 0.8, 0.025], frame_on=True, sharex=ax_dendro)
-        error_viewer = ErrorViewer(self.model)
-        error_viewer.draw_error(ax_error)
-        error_viewer.draw_xlabels(ax_error)
-
-        self.ax_label = ax_error
-
-        plt.setp(ax_dendro.get_xticklabels(), visible=False)
-        plt.setp(ax_clone.get_xticklabels(), visible=False)
-        plt.setp(ax_clone.get_xticklines(), visible=False)
-        plt.setp(ax_subclone.get_xticklabels(), visible=False)
-        plt.setp(ax_subclone.get_xticklines(), visible=False)
-        plt.setp(ax_heat.get_xticklabels(), visible=False)
-        plt.setp(ax_sector.get_xticklabels(), visible=False)
-        plt.setp(ax_gate.get_xticklabels(), visible=False)
-        plt.setp(ax_multiplier.get_xticklabels(), visible=False)
-
-        self.sample_viewer = SampleViewer(self.model)
-        self.controller.event_loop_connect(self.fig)
-        self.main.refresh()
-
-
-def main():
-    root = tk.Tk()
-    root.wm_title("cnviewer")
-
-    controller = MainController()
-    main = MainWindow(root, controller)
-    main.build_ui()
-
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(0, weight=1)
-
-    root.mainloop()
+from cnviewer.main_window import MainWindow
 
 
 def show_single_sector(model, sector_id):
@@ -144,6 +22,20 @@ def show_single_sector(model, sector_id):
     controller.build_sector(main.fig)
 
     main.connect_controller(controller)
+
+    root.mainloop()
+
+
+def main():
+    root = tk.Tk()
+    root.wm_title("cnviewer")
+
+    controller = MainController()
+    main = MainWindow(root, controller)
+    main.build_ui()
+
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
 
     root.mainloop()
 
