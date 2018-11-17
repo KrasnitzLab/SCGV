d31da7cd8de467e38a63346f9527c158f8eb1a1f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-11-17 11:36:17 +0200

    initial pyqt main window added...

---
+++ scgv/qtviews/main_window.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+
+from matplotlib.backends.backend_qt5agg import FigureCanvas, \
+    NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("SCGV Main")
+
+        self._main = QWidget()
+        self.setCentralWidget(self._main)
+        layout = QVBoxLayout(self._main)
+
+        static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        layout.addWidget(static_canvas)
+        self.addToolBar(NavigationToolbar(static_canvas, self))
+
+        self._static_ax = static_canvas.figure.subplots()
+        t = np.linspace(0, 10, 501)
+        self._static_ax.plot(t, np.tan(t), ".")
