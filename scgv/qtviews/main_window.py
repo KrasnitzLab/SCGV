9a37c1a4b841c133b95281bc50f86b927e6ed667
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-11-17 12:08:51 +0200

    open directory and archive button added...

--- scgv/qtviews/main_window.py
+++ scgv/qtviews/main_window.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QAction, \
+    QStatusBar
+from PyQt5.QtGui import QIcon
 
 from matplotlib.backends.backend_qt5agg import FigureCanvas, \
     NavigationToolbar2QT as NavigationToolbar
@@ -20,8 +22,32 @@ class MainWindow(QMainWindow):
 
         static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
         layout.addWidget(static_canvas)
-        self.addToolBar(NavigationToolbar(static_canvas, self))
+
+        self.toolbar = NavigationToolbar(static_canvas, self)
+        self.addToolBar(self.toolbar)
 
         self._static_ax = static_canvas.figure.subplots()
         t = np.linspace(0, 10, 501)
         self._static_ax.plot(t, np.tan(t), ".")
+
+        self.setStatusBar(QStatusBar(self))
+
+        self.toolbar.addSeparator()
+
+        open_dir_action = QAction(
+            QIcon("icons/folder.png"), "Open Directory", self)
+        open_dir_action.setStatusTip("Open Directory button")
+        open_dir_action.triggered.connect(self.on_open_directory_click)
+        self.toolbar.addAction(open_dir_action)
+
+        open_archive_action = QAction(
+            QIcon("icons/archive.png"), "Open Archive", self)
+        open_archive_action.setStatusTip("Open Archive button")
+        open_archive_action.triggered.connect(self.on_open_archive_click)
+        self.toolbar.addAction(open_archive_action)
+
+    def on_open_directory_click(self, s):
+        print("click open directory", s)        
+    
+    def on_open_archive_click(self, s):
+        print("click open archive", s)
