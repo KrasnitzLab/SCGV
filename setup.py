9a94704a5234c3042a9966a30b846e9a64cc650a
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2020-03-05 11:47:10 +0200

    long description changed...

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="scgv",
     version="1.1.0",
@@ -11,8 +8,24 @@ setuptools.setup(
     author_email="lubomir.chorbadjiev@gmail.com",
     description="SCGV is an interactive graphical tool for single-cell "
     "genomics data, with emphasis on single-cell genomics of cancer",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description="""SCGV is an interactive graphical tool for 
+        single-cell genomics data, with
+        emphasis on single-cell genomics of cancer. It facilitates examination, jointly
+        or individually, of DNA copy number profiles of cells harvested from
+        multiple anatomic locations (sectors). In the opening view the copy-number
+        data matrix, with columns corresponding to cells and rows to genomic locations,
+        is represented as a heat map with color-encoded integer DNA copy number. If a
+        phylogenetic tree is available for the cells comprising the dataset, it can be
+        used to order the columns of the data matrix, and clones formed by closely
+        related cells may be identified. Alternatively, the columns
+        can be ordered by the sector of origin of the cells. Cyto-pathological
+        information may be displayed in a separate view, including sector-specific
+        slide images and pathology reports. Genomic sub-regions and
+        random subsets of cells can be selected and zoomed into. Individual or multiple
+        copy-number profiles may be plotted as copy number against the genomic
+        coordinate, and these plots may again be zoomed into. Chromosomal regions
+        selected within the profiles may be followed to UCSC genome browser to
+        examine the genomic context.""",
     url="https://github.com/KrasnitzLab/SCGV",
     packages=setuptools.find_packages(
         # 'scgv',
